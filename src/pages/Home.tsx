40fdeb210a63abfc957271defdd8dfa2101bc2ac
kdjmg <khodja.ghenia@gmail.com>
2024-08-11 17:41:09 +0200

    update btn cmd

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,16 +1,22 @@
 import { ImageCarousel } from "../components/ImageCarousel";
 import { Section1 } from "../components/Section1";
 import { Section2 } from "../components/Section2";
+import { useNavigate } from 'react-router-dom';
+
 
 
 
 export function Home() {
-
+    const navigate = useNavigate();
+  
+    const handleButtonClick = () => {
+      navigate('/menu');
+    }
   return (
     <>
     <div className="relative w-full mb-4">
       <img src="/ppt2.jpeg" alt="" className=" w-full h-96 object-cover rounded-b-lg" />
-      <button className="absolute bg-blue-950 text-white px-6 py-3 rounded-lg shadow-lg transition duration-300 ease-in-out  hover:bg-blue-900 hover:scale-105 hover:shadow-xl left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2">
+      <button onClick={handleButtonClick} className="absolute bg-blue-950 text-white px-6 py-3 rounded-lg shadow-lg transition duration-300 ease-in-out  hover:bg-blue-900 hover:scale-105 hover:shadow-xl left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2">
     Commander
   </button>
 
